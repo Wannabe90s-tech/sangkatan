1dd6c987a49b745b2d85e00b764fa82a1e01872a
Supachai Misa <69750795+supachaimisa@users.noreply.github.com>
2021-03-25 18:07:32 +0700

    first commit

---
+++ application/views/home/home.php
@@ -0,0 +1,201 @@
+<link rel="stylesheet" href="<?php echo base_url() . 'css/home.css' ?>">
+<!-- style="background-color: #FFFEAF;" -->
+<div class="container mt-5">
+  <div class="row" style="background-color: #DADADA; padding: 9px;">
+    <div class="col-md-12">
+      <span style="padding: 8px;">เรียงโดย</span>
+      <span style="background-color: white; padding: 8px;">เกี่ยวข้อง</span>
+      <span style="background-color: white; padding: 8px;">ล่าสุด</span>
+      <span style="background-color: white; padding: 8px;">สินค้าขายดี</span>
+      <select class="filter-search" type="text" style="padding: 7px; border: none;" placeholder="ราคา">
+        <option value="">ราคา</option>
+        <option value="">ราคา - จากน้อยไปมาก</option>
+        <option value="">ราคา - จากมากไปน้อย</option>
+      </select>
+    </div>
+  </div>
+  <!-- <div class="filter-search-list">
+          <p></p>
+          <p>รายการที่2</p>
+          <p>รายการที่3</p>
+      </div> -->
+  <div class="row">
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . 'assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-2 mt-3">
+      <div class="card">
+        <img src="<?php echo base_url() . '/assets/img/1234.jpg' ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h6 class="card-title">สังฆทานประจำวันจันทร์</h6>
+          <div class="price">
+            <span class="out-price" style="color: #898989;"><s>150฿</s></span>
+            <span class="net-price" style="color: tomato;">100฿</span>
+          </div>
+          <div class="promotion">
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาพิเศษ</span>
+            <span style="border: 1px solid red; font-size: 8pt;">ราคาปกติ</span>
+          </div>
+          <div class="popular" style="font-size: 10pt;">
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+            <i class="fas fa-star" style="color:tomato;"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
